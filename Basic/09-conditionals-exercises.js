f570e4ddecd722d262e197556dd36fe290098f9e
Mvillegas18 <108032325+Mvillegas18@users.noreply.github.com>
2024-11-29 23:19:48 +0000

    any completed exercise conditionals

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -6,15 +6,52 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=8652
 // if/else/else if/ternaria
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
+    const variable = "hola"
+
+    if(variable === "hola"){
+        console.log("Mi nombre es Miguel")
+    }
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+    const contraseña = "agfdsfresc"
+    const usuario = "Miguel"
+    if(contraseña === "agfdsfresc" && usuario === "Miguel"){
+        console.log("Haz iniciado sesion");
+        
+    }else{
+        console.log("Contraseño incorrecta");
+        
+    }
 
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
+    const numero = -10;
+    if(numero > 0){
+        console.log("El numero es positivo");
+        
+    }else if(numero == 0){
+        console.log("El numero es cero");
+        
+    }else{
+        console.log("El numero es negativo");
+        
+    }
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
 
+    const edad = 20;
+    if(edad >= 18){
+        console.log("Tienes perimitido votar, tienes " + edad);
+        
+    }else{
+        const añosFaltantes = 18 - edad
+        console.log(`Eres menor de edad. Te hacen falta ${añosFaltantes} años`);
+        
+    }
+
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
-//    dependiendo de la edad 
+//    dependiendo de la edad
+    const persona = edad < 18 ? "menor" : "Adulto"
+    console.log(persona)
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
 
@@ -23,6 +60,22 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=8652
 // switch
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+    const idioma = "español"
+
+    switch(idioma){
+        case "español":
+            console.log("Hola");
+            break;
+        case "ingles":
+            console.log("Hello");
+            break;
+        case "frances":
+            console.log("Bonjour");
+            break;
+        default:
+            console.log("Idioma no soportado");
+            break;
+    }
 
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
 
